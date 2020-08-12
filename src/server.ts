9d8587fa051d54ee94d8aecc10c3426e3e530f79
Joao-rangel <joao.vran@gmail.com>
2020-08-12 15:29:30 -0300

    db class configuration

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
 import express from 'express'
+import routes from './routes'
 
 const app = express() // declarou variavel app (aplicação) e chamou a função express
 
 app.use(express.json()) // fala ao ts para usar json
-
-app.get('/', (request, response) => {
-
-  return response.json({users: 'sou um usuário'})
-})
+app.use(routes)
 
 app.listen(3333) // ouve (vigia, espera) requisições http
